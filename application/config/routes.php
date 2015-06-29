47484910041ed8d518494f3512d563f768d1194e
Supat <pat154154@gmail.com>
2015-06-29 20:37:28 +0700

    Add route

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'fontend/home/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['admin'] = 'backoffice/dashboard';
+$route['admin/(:any)'] = 'backoffice/$1';
+$route['admin/(:any)/(:any)'] = 'backoffice/$1/$2';
+
+$route['(:any)'] = 'fontend/$1';
+$route['(:any)/(:any)'] = 'fontend/$1/$2';
