986a318b44e15a8596e1253c66f60813285abd0a
Supat <pat154154@gmail.com>
2015-07-21 21:42:44 +0700

    add
    Model

--- application/modules/api/controllers/comment.php
+++ application/modules/api/controllers/comment.php
@@ -2,9 +2,58 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class comment extends CI_Controller {
 
-    public function index()
-    {
-        echo 'api';
-//		$this->load->view('welcome_message');
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('model_global_core','core');
+    }
+
+    public function comment_view(){
+        $config = array(
+            'database' => 'default',
+            'from' => 'comments',
+            'where' => array()
+        );
+        $this->core->_get($config);
+
+    }
+
+    public function comment_create(){
+        $config = array(
+            'database' => 'default',
+            'from' => 'comments',
+            'data' => array(
+
+            )
+        );
+        $this->core->_set($config);
+
+    }
+
+    public function comment_edit(){
+        $config = array(
+            'database' => 'default',
+            'from' => 'comments',
+            'data' => array(
+
+            )
+        );
+        $this->core->_edit($config);
+
+    }
+
+    public function comment_delete(){
+        $config = array(
+            'database' => 'default',
+            'from' => 'comments',
+            'data' => array(
+
+            )
+        );
+        $this->core->_erase($config);
+
+    }
+
+    public function like(){
+
     }
 }
